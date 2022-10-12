215f70717bf6477ef02772176c8373a060baa494
Malay Satiya <malaysatiya19@gmail.com>
2022-10-12 16:28:32 +0530

    Added User related Changes Added UserController and UesrService with CRUD Added User Model Updated ApplicationDbContext and ConnectionString

---
+++ Target-Setting-Tool.Web/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Target_Setting_Tool.Web.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        [ForeignKey("Role")]
+        public Guid RoleId { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string EmployeeCode { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedDate { get; set; }
+        public Guid CreatedBy { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime? ModifiedDate { get; set; }
+        public Guid? ModifiedBy { get; set; }
+        public bool IsDeleted { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime? DeletedDate { get; set; }
+        public Guid? DeletedBy { get; set; }
+        public virtual Roles Role { get; set; }
+    }
+}
---
+++ Target-Setting-Tool.Web/Services/UserServices/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Target_Setting_Tool.Web.Models;
+
+namespace Target_Setting_Tool.Web.Services.UserServices
+{
+    public interface IUserService
+    {
+        public Task<bool> AddUser(User user);
+        public Task<List<User>> GetAllUser();
+        public Task<User> GetUserById(Guid id);
+        public Task<User> GetUserByEmail(string email);
+        public Task<User> GetUserByEmployeeCode(string employeeCode);
+        public Task<bool> UpdateUser(User user);
+        public Task<bool> DeleteUser(Guid id);
+    }
+}
